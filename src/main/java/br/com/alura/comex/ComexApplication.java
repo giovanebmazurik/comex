c9ec45212edcb4b66ff69e6e4e9f086c2743693b
Giovane Mazurik <giovane.mazurik@cits.br>
2022-10-26 09:04:15 -0300

    Including Entitys

--- src/main/java/br/com/alura/comex/ComexApplication.java
+++ src/main/java/br/com/alura/comex/ComexApplication.java
@@ -1,4 +1,4 @@
-package br.com.alura.comex.comex;
+package br.com.alura.comex;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/br/com/alura/comex/ComexApplicationTests.java
+++ src/test/java/br/com/alura/comex/ComexApplicationTests.java
@@ -1,4 +1,4 @@
-package br.com.alura.comex.comex;
+package br.com.alura.comex;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
