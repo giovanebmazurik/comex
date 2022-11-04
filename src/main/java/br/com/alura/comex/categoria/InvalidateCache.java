32f0676f6b6251e94f385d3ad762b40cc45a369d
Giovane Mazurik <giovane.mazurik@cits.br>
2022-11-04 20:43:34 -0300

    Atividade3 - Actuator

--- src/main/java/br/com/alura/comex/categoria/InvalidateCache.java
+++ src/main/java/br/com/alura/comex/categoria/InvalidateCache.java
@@ -1,19 +1,20 @@
 package br.com.alura.comex.categoria;
 
-import br.com.alura.comex.cliente.dto.ClienteOutputDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
+@Tag(name = "Cache")
 @RestController
 @RequestMapping("aW52YWxpZGEgcmVsYXTDs3JpbyBkZSB2ZW5kYXM")
 @CacheEvict(value = "relatorioPedidos", allEntries = true)
 public class InvalidateCache {
 
+    @Operation(summary = "Invalidate")
     @GetMapping
     public ResponseEntity<?> invalidate(){
         return null;
